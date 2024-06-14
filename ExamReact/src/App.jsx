a470ff0812bf0b841625d7fbc2b7bdd28f05cf28
LauraSdt <lauramarie.sdtgmail.com>
2024-06-14 14:35:40 +0200

    changements mineurs

--- ExamReact/src/App.jsx
+++ ExamReact/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react";
 import ProductList from "./Components/ProductList";
 import ProductForm from "./Components/ProductForm";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import "./app.css";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const notify = () => toast("Wow so easy!");
 
   useEffect(() => {
     console.log("UseEffect");
@@ -20,8 +17,6 @@ const App = () => {
   return (
     <div className="addProduct">
       <h1>Product list</h1>
-      <button onClick={notify}>Let's add a product !!</button>
-      <ToastContainer />
       <ProductForm addProduct={addProduct} />
       <ProductList products={products} />
     </div>
